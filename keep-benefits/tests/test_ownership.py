0d246637a0c20bba46dc619d03124d1f52c7e867
Leibniz137 <nate@thenateway.io>
2020-12-19 12:52:08 -0800

    update ownership test
    anyone can call methods, but rewards always go to owner

--- keep-benefits/tests/test_ownership.py
+++ keep-benefits/tests/test_ownership.py
@@ -1,14 +1,17 @@
-from brownie.exceptions import VirtualMachineError
-import pytest
-
-
 def test_non_owner_withdraw_eth(accounts, Beneficiary):
     deployer = accounts[0]
+
+    starting_balance = deployer.balance()
 
     beneficiary = Beneficiary.deploy(
         '0xdF708431162Ba247dDaE362D2c919e0fbAfcf9DE', {'from': deployer})
 
-    accounts[1].transfer(beneficiary, "10 ether", gas_price=0)
+    non_owner = accounts[1]
+    non_owner.transfer(beneficiary, "10 ether", gas_price=0)
 
-    with pytest.raises(VirtualMachineError):
-        beneficiary.withdrawEth(10**18, {'from': accounts[1]})
+    tx = beneficiary.withdrawEth(10**18, {'from': non_owner})
+
+    # transaction is successful
+    assert tx.status == 1
+
+    assert deployer.balance() == starting_balance + "1 ether"
